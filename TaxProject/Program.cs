eac2fe979b113558c98cf39a096689cf30ba2da2
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-18 23:19:23 +0300

    [Tax] replace explicit types with var

--- TaxProject/Program.cs
+++ TaxProject/Program.cs
@@ -4,9 +4,9 @@
     {
         private static void Main(string[] args)
         {
-            Product product = new Product("Milk", 20.25m, 123456);
-            ITax tax = new Tax();
-            IPriceCalculator priceCalculator = new PriceCalculator(tax);
+            var product = new Product("Milk", 20.25m, 123456);
+            var tax = new Tax();
+            var priceCalculator = new PriceCalculator(tax);
             Console.WriteLine($"Price before Tax : {product.Price}");
             Console.WriteLine($"Price after Tax : {priceCalculator.CalculateTotalPrice(product)}");
 
