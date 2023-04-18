4b1942be5476793c4b0da36445acd4ca560dc616
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-18 14:24:25 +0300

    [Tax] PriceCalculator and interface to handle calculating the Price

--- TaxProject/Program.cs
+++ TaxProject/Program.cs
@@ -4,9 +4,12 @@
     {
         private static void Main(string[] args)
         {
-            Product product = new Product("Milk", 2.99m, 123456);
+            Product product = new Product("Milk", 20.25m, 123456);
             ITax tax = new Tax();
-            
+            IPriceCalculator priceCalculator = new PriceCalculator(tax);
+            Console.WriteLine($"Price before Tax : {product.Price}");
+            Console.WriteLine($"Price after Tax : {priceCalculator.CalculateTotalPrice(product)}");
+
         }
     }
 }
