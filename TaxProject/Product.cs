77286eb77e05a748e404239bac63b12b01132b95
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-18 14:10:07 +0300

    [TAX] Create Product Class

---
+++ TaxProject/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace TaxProject
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        private decimal _price;
+
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Price cannot be negative");
+                }
+                _price = value;
+            }
+        }
+
+        public int UPC { get; set; }
+        public Product(string name, decimal price, int uPC)
+        {
+            Name = name;
+            Price = price;
+            UPC = uPC;
+        }
+
+    }
+}
