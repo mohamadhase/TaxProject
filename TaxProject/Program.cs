d8be9547af01b6362fce04421a9908ccf59df7b9
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-18 13:52:43 +0300

    Add project files.

---
+++ TaxProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaxProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
