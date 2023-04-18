3402693e1aca9854c775e84580bdb027dfd7ad09
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-18 23:43:09 +0300

    [Discount] Add the DiscountService to the PriceCalculator Class

--- TaxProject/Program.cs
+++ TaxProject/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TaxProject
+﻿using TaxProject.services;
+
+namespace TaxProject
 {
     internal class Program
     {
@@ -6,9 +8,10 @@
         {
             var product = new Product("Milk", 20.25m, 123456);
             var tax = new TaxService();
-            var priceCalculator = new PriceCalculator(tax);
-            Console.WriteLine($"Price before Tax : {product.Price}");
-            Console.WriteLine($"Price after Tax : {priceCalculator.CalculateTotalPrice(product)}");
+            var discount = new DiscountService(0.15m);
+            var priceCalculator = new PriceCalculator(tax,discount);
+            Console.WriteLine($"Price before Tax and Discount : ${product.Price}");
+            Console.WriteLine($"Price after Tax And Discount : ${priceCalculator.CalculateTotalPrice(product)}");
 
         }
     }
--- TaxProject/services/PriceCalculator.cs
+++ TaxProject/services/PriceCalculator.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaxProject.interfaces;
 
 namespace TaxProject
 {
     public class PriceCalculator : IPriceCalculator
     {
         private readonly ITaxService _tax;
-        public PriceCalculator(ITaxService tax)
+        private readonly IDiscountService _discount;
+        public PriceCalculator(ITaxService tax,IDiscountService discount)
         {
             _tax = tax;
+            _discount = discount;
         }
         public decimal CalculateTotalPrice(Product product)
         {
-            return product.Price + _tax.GetTaxAmount(product.Price);
+            var price = product.Price;
+            var taxAmount = _tax.GetTaxAmount(price);
+            var discountAmount = _discount.GetDiscountAmuont(price);
+            var totalPrice = price + taxAmount - discountAmount;
+            return totalPrice;
         }
     }
 }
