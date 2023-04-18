6dc3556c5c0e6fcd515c5cb77221a97ec3445fb6
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-18 23:38:49 +0300

    [Discount] create DiscountService  and IDiscountService interface

---
+++ TaxProject/services/Discount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxProject.interfaces;
+namespace TaxProject.services
+{
+ 
+
+    public class DiscountService : IDiscountService
+    {
+        public decimal DiscountPercentage { get; set; }
+        public DiscountService(decimal discountPercentage)
+        {
+            DiscountPercentage = discountPercentage;
+        }
+
+        public decimal GetDiscountAmuont(decimal price)
+        {
+            return Math.Round(price * DiscountPercentage, 2);
+        }
+    }
+}
