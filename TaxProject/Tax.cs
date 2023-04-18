5006dfd59227c65cf0c22b87702e82cc86ccc6ce
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-18 14:23:58 +0300

    [Tax] Create Constructor and defult value for the Tax

--- TaxProject/Tax.cs
+++ TaxProject/Tax.cs
@@ -3,6 +3,11 @@
     public class Tax : ITax
     {
         public decimal TaxPercentage { get; set; }
+        public Tax(decimal taxPercentage = 0.20m)
+        {
+            TaxPercentage = taxPercentage;
+        }
+
         public decimal GetTaxAmount(decimal price)
         {
             return Math.Round(price * TaxPercentage,2);
