50095a34bfd98767d9ad47781e7d3f990144f2c2
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-21 01:04:20 +0300

    [SELECTIVE] Create DiscountCalculator class and interface to handle calculating the both types of discount for given price

---
+++ TaxProject/services/DiscountCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxProject.interfaces;
+
+namespace TaxProject.services
+{
+    public class DiscountCalculator : IDiscountCalculator
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISelectiveDiscount _selectiveDiscount;
+
+        public DiscountCalculator(IDiscountService discountService, ISelectiveDiscount selectiveDiscount)
+        {
+            _discountService = discountService;
+            _selectiveDiscount = selectiveDiscount;
+        }
+
+        public decimal GetTotalDiscount(decimal price, int upc)
+        {
+            var discountAmount = _discountService.GetDiscountAmuont(price);
+            var selectiveDiscountAmount = _selectiveDiscount.GetDiscountAmount(price, upc);
+            return discountAmount + selectiveDiscountAmount;
+
+        }
+    }
+}
