391da3073361c6edffafb60a04da90f72b1a7dac
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-21 00:57:43 +0300

    [SELECTIVE] Create Selective Discount Calss and Interface

---
+++ TaxProject/services/SelectiveDiscount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxProject.interfaces;
+
+namespace TaxProject.services
+{
+    public class SelectiveDiscount : ISelectiveDiscount
+    {
+        public decimal DiscountPercentage { get; set; }
+        public int UPS { get; set; }
+
+        public SelectiveDiscount(decimal discountPercentage, int uPS)
+        {
+            DiscountPercentage = discountPercentage;
+            UPS = uPS;
+        }
+
+        public decimal GetDiscountAmount(decimal price, int ups)
+        {
+            if (UPS != ups)
+            {
+                return 0;
+            }
+            return Math.Round(price * DiscountPercentage, 2);
+        }
+
+    }
+}
