4161c69280d860d962f404a112be679d0846eedf
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-22 09:05:50 +0300

    [EXPENSES] Create Absolute and Pecentage Expenses classes inhereted from the ExpenseBase class

---
+++ TaxProject/models/AbsoluteExpense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxProject.models
+{
+    public class AbsoluteExpense : BaseExpense
+    {
+        public AbsoluteExpense(string description, decimal amount) : base(description, amount)
+        {
+        }
+
+        public override decimal GetCost(decimal price)
+        {
+            return Amount;
+        }
+    }
+}
