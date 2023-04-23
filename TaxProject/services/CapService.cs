42b955f7048bac3079f9036187efb42050b60cb5
Mohammad nasser <76165108+mohamadhase@users.noreply.github.com>
2023-04-23 21:26:11 +0300

    [CAP] Create CapService class and interface

---
+++ TaxProject/services/CapService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxProject.interfaces;
+
+namespace TaxProject.services
+{
+    public class CapService : ICapService
+    {
+        public decimal CapAmount { get; set; }
+
+        public CapService(decimal amount)
+        {
+            CapAmount = amount;
+        }
+
+        public decimal ApplyCap(decimal amount)
+        {
+            var cap = CapAmount;
+            if (CapAmount <= 1)
+            {
+                cap = CapAmount * amount;
+            }
+            if (amount >= cap)
+            {
+                return cap;
+            }
+            return amount;
+        }
+    }
+}
